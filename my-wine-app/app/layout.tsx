677aa5f3a4e443b2de03c189bf1f757a52065b1c
EightonLeePaulse12 <eightonleepaulse@gmail.com>
2024-01-12 13:14:40 +0200

    Navbar and Footer in working progress

--- my-wine-app/app/layout.tsx
+++ my-wine-app/app/layout.tsx
@@ -1,24 +1,10 @@
 import type { Metadata } from "next";
-import { useRouter } from "next/router";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
-import useMetadata from 'next/head'
 
 export const metadata: Metadata = {
   title: "My Wines App",
   description: "Managing wine with quality",
-};
-
-const Title = () => {
-  const router = useRouter();
-  const { pathname } = router;
-  const metadata = useMetadata()
-
-  const page = pathname.split("/").pop() || "";
-
-  metadata.title = `${page} - My Wines App`;
-
-  return null;
 };
 
 export default function RootLayout({
@@ -34,5 +20,3 @@ export default function RootLayout({
     </>
   );
 }
-
-export { Title };
--- my-wine-app/tailwind.config.ts
+++ my-wine-app/tailwind.config.ts
@@ -1,12 +1,12 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: [
+ content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
+ ],
+ theme: {
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
@@ -14,7 +14,7 @@ const config: Config = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
-  },
-  plugins: [],
+ },
+ plugins: [],
 }
 export default config
